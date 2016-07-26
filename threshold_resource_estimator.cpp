f2a79a1dc321c986e64f84a63b1cda9268340853
Matthias Bach <matthias.bach@blue-yonder.com>
2016-07-26 17:02:06 +0200

    Implement non-functional requirement to operate non-blocking

--- threshold_resource_estimator.cpp
+++ threshold_resource_estimator.cpp
@@ -1,23 +1,72 @@
+#include <process/dispatch.hpp>
+#include <process/process.hpp>
+
 #include <mesos/module/resource_estimator.hpp>
 
+using process::dispatch;
+using process::Failure;
 using process::Future;
+using process::Process;
+using process::Owned;
 
 using mesos::Resources;
 using mesos::ResourceUsage;
 
 namespace {
 
+class ThresholdResourceEstimatorProcess : public Process<ThresholdResourceEstimatorProcess>
+{
+public:
+    Future<Resources> oversubscribable();
+};
+
 class ThresholdResourceEstimator : public mesos::slave::ResourceEstimator
 {
-    virtual Try<Nothing> initialize(const std::function<Future<ResourceUsage>()>&) final {
-        return Nothing();
-    }
+public:
+    virtual Try<Nothing> initialize(const std::function<Future<ResourceUsage>()>&) final;
 
-    virtual Future<Resources> oversubscribable() final {
-        return Resources();
-    }
+    virtual Future<Resources> oversubscribable() final;
 
+    virtual ~ThresholdResourceEstimator();
+
+protected:
+    Owned<ThresholdResourceEstimatorProcess> process;
 };
+
+
+Future<Resources> ThresholdResourceEstimatorProcess::oversubscribable()
+{
+    return Resources();
+}
+
+
+Try<Nothing> ThresholdResourceEstimator::initialize(const std::function<Future<ResourceUsage>()>&) {
+    if (process.get() != nullptr) {
+        return Error("Threshold resource estimator has already been initialized");
+    }
+
+    process.reset(new ThresholdResourceEstimatorProcess());
+    spawn(process.get());
+
+    return Nothing();
+}
+
+Future<Resources> ThresholdResourceEstimator::oversubscribable() {
+    if (process.get() == nullptr) {
+        return Failure("Threshold resource estimator is not initialized");
+    }
+
+    return dispatch(process.get(), &ThresholdResourceEstimatorProcess::oversubscribable);
+}
+
+ThresholdResourceEstimator::~ThresholdResourceEstimator()
+{
+    if (process.get() != nullptr) {
+        terminate(process.get());
+        wait(process.get());
+    }
+}
+
 
 static mesos::slave::ResourceEstimator* create(const mesos::Parameters& parameters) {
     return new ThresholdResourceEstimator();
