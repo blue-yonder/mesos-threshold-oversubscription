aea740487ee602bf76306d11f3bd8f152593439f
Matthias Bach <matthias.bach@blue-yonder.com>
2016-07-27 12:34:27 +0200

    Add test to ensure non-revocable allocations are ignored

--- threshold_resource_estimator_test.cpp
+++ threshold_resource_estimator_test.cpp
@@ -120,14 +120,22 @@ public:
         : value{std::make_shared<ResourceUsage>()}
     {};
 
-    void set(std::string const revocable_allocated) {
+    void set(std::string const & revocable_allocated, std::string const & non_revocable_allocated) {
         value->Clear();
         auto * revocable_executor = value->add_executors();
-        auto resources = Resources::parse(revocable_allocated);
-        for(auto const & parsed_resource: resources.get()) {
+        auto revocable_resources = Resources::parse(revocable_allocated);
+        for(auto const & parsed_resource: revocable_resources.get()) {
             auto * mutable_resource = revocable_executor->add_allocated();
             mutable_resource->CopyFrom(parsed_resource);
             mutable_resource->mutable_revocable();  // mark as revocable
+        }
+
+        auto * non_revocable_executor = value->add_executors();
+        auto non_revocable_resources = Resources::parse(non_revocable_allocated);
+        for(auto const & parsed_resource: non_revocable_resources.get()) {
+            auto * mutable_resource = non_revocable_executor->add_allocated();
+            mutable_resource->CopyFrom(parsed_resource);
+            mutable_resource->clear_revocable();  // mark as non-revocable
         }
     }
     Future<ResourceUsage> operator()() const {
@@ -139,21 +147,25 @@ private:
 
 TEST(UsageMockTest, test_set) {
     UsageMock mock;
-    mock.set("cpus:3;mem:62");
+    mock.set("cpus:3;mem:62", "cpus:1;mem:48");
 
     ResourceUsage usage = mock().get();
     Resources allocatedRevocable;
+    Resources allocatedNonRevocable;
     for(auto & executor: usage.executors()) {
         allocatedRevocable += Resources(executor.allocated()).revocable();
+        allocatedNonRevocable += Resources(executor.allocated()).nonRevocable();
     }
 
-    EXPECT_EQ(1, usage.executors_size());
+    EXPECT_EQ(2, usage.executors_size());
     EXPECT_EQ(3, allocatedRevocable.cpus().get());
     EXPECT_EQ(Bytes::parse("62MB").get(), allocatedRevocable.mem().get());
+    EXPECT_EQ(1, allocatedNonRevocable.cpus().get());
+    EXPECT_EQ(Bytes::parse("48MB").get(), allocatedNonRevocable.mem().get());
 
     // must also work for copy
     UsageMock copy = mock;
-    mock.set("cpus:5;mem:127");
+    mock.set("cpus:5;mem:127", "");
     usage = copy().get();
 
     Resources copiedRevocable;
@@ -191,10 +203,17 @@ TEST_F(ThresholdResourceEstimatorTest, test_underutilized) {
     EXPECT_EQ(2.0, available_resources.revocable().cpus().get());
     EXPECT_EQ(Bytes::parse("512MB").get(), available_resources.revocable().mem().get());
 
-    usage.set("cpus(*):1.5;mem(*):128");
+    // Usage of revocable resources reduces offers
+    usage.set("cpus(*):1.5;mem(*):128", "");
     available_resources = estimator->oversubscribable().get();
     EXPECT_EQ(0.5, available_resources.revocable().cpus().get());
     EXPECT_EQ(Bytes::parse("384MB").get(), available_resources.revocable().mem().get());
+
+    // Usage of non-revocable resources is ignored
+    usage.set("", "cpus(*):1.5;mem(*):128");
+    available_resources = estimator->oversubscribable().get();
+    EXPECT_EQ(2.0, available_resources.revocable().cpus().get());
+    EXPECT_EQ(Bytes::parse("512MB").get(), available_resources.revocable().mem().get());
 
     // TODO also test with actual usage at threshold
 }
