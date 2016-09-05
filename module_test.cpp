848bfb529ac3295cab81d0aefd15a50235b4cf1c
Stephan Erb <stephan.erb@blue-yonder.com>
2016-09-05 11:43:50 +0200

    Make module_test code reusable

--- module_test.cpp
+++ module_test.cpp
@@ -24,12 +24,13 @@ using mesos::slave::ResourceEstimator;
 
 namespace {
 
-string const libraryName{"threshold_resource_estimator"};
-string const moduleName{"com_blue_yonder_ThresholdResourceEstimator"};
+string const estimatorLibraryName{"threshold_resource_estimator"};
+string const estimatorModuleName{"com_blue_yonder_ThresholdResourceEstimator"};
 
-class ThresholdResourceEstimatorTest : public ::testing::Test {
+
+class ModuleTest : public ::testing::Test {
 public:
-    ThresholdResourceEstimatorTest()
+    ModuleTest()
         : dynamicLibrary(new DynamicLibrary()),
           moduleBase(nullptr)
     { }
@@ -42,13 +43,83 @@ public:
         ::testing::Test::TearDown();
     }
 
-    Try<ModuleBase*> loadModule();
-    ResourceEstimator* createEstimator(mesos::Parameters const & params);
+    Try<ModuleBase*> loadModule(string const &, string const &);
 
 private:
     Owned<DynamicLibrary> dynamicLibrary;
     ModuleBase* moduleBase;
 };
+
+Try<ModuleBase*> ModuleTest::loadModule(
+        string const & libraryName,
+        string const & moduleName)
+{
+    if (this->moduleBase == nullptr) {
+        auto const path = "./" + os::libraries::expandName(libraryName);
+        Try<Nothing> result = this->dynamicLibrary->open(path);
+        if (!result.isSome()) {
+            return Error("Error opening library of module: '" + moduleName + "': " + result.error());
+        }
+
+        Try<void*> symbol = this->dynamicLibrary->loadSymbol(moduleName);
+        if (symbol.isError()) {
+            return Error("Error loading module '" + moduleName + "': " + symbol.error());
+        }
+
+        this->moduleBase = reinterpret_cast<ModuleBase*>(symbol.get());
+    }
+    return moduleBase;
+}
+
+
+class ThresholdResourceEstimatorTest : public ModuleTest {
+public:
+    ThresholdResourceEstimatorTest() : ModuleTest() { }
+
+    Try<ModuleBase*> loadModule();
+    ResourceEstimator* createEstimator(mesos::Parameters const & params);
+
+};
+
+Try<ModuleBase*>  ThresholdResourceEstimatorTest::loadModule() {
+    return ModuleTest::loadModule(estimatorLibraryName, estimatorModuleName);
+}
+
+ResourceEstimator* ThresholdResourceEstimatorTest::createEstimator(mesos::Parameters const & params) {
+    auto load_result = this->loadModule();
+    ModuleBase* moduleBase = load_result.get();
+    auto estimatorModule = reinterpret_cast<Module<ResourceEstimator>*>(moduleBase);
+    return estimatorModule->create(params);
+}
+
+void verifyModule(const string& moduleName, const ModuleBase* moduleBase)
+{
+    ASSERT_TRUE(moduleBase != nullptr);
+    ASSERT_TRUE(moduleBase->mesosVersion != NULL) << "Module " << moduleName << " is missing field 'mesosVersion'";
+    ASSERT_TRUE(moduleBase->moduleApiVersion != NULL)
+        << "Module " << moduleName <<" is missing field 'moduleApiVersion'";
+    ASSERT_TRUE(moduleBase->authorName != NULL) << "Module " << moduleName << " is missing field 'authorName'";
+    ASSERT_TRUE(moduleBase->authorEmail != NULL) << "Module " << moduleName << " is missing field 'authoEmail'";
+    ASSERT_TRUE(moduleBase->description != NULL) << "Module " << moduleName << " is missing field 'description'";
+    ASSERT_TRUE(moduleBase->kind != NULL) << "Module " << moduleName << " is missing field 'kind'";
+
+    // Verify module API version.
+    EXPECT_TRUE(stringify(moduleBase->moduleApiVersion) == MESOS_MODULE_API_VERSION)
+        << "Module API version mismatch. Mesos has: " MESOS_MODULE_API_VERSION ", "
+        << "library requires: " + stringify(moduleBase->moduleApiVersion);
+
+    EXPECT_EQ("ResourceEstimator", stringify(moduleBase->kind));
+
+    Try<Version> mesosVersion = Version::parse(MESOS_VERSION);
+
+    Try<Version> moduleMesosVersion = Version::parse(moduleBase->mesosVersion);
+    EXPECT_FALSE(moduleMesosVersion.isError()) << moduleMesosVersion.error();
+
+    EXPECT_EQ(moduleMesosVersion.get(), mesosVersion.get())
+        << "Module is not compiled for the current Mesos version.";
+
+    EXPECT_TRUE(moduleBase->compatible()) << "Module " << moduleName << "has determined to be incompatible";
+}
 
 mesos::Parameters make_parameters(
     std::string fixed,
@@ -89,60 +160,6 @@ mesos::Parameters make_parameters(
     return parameters;
 }
 
-void verifyModule(const string& moduleName, const ModuleBase* moduleBase)
-{
-    ASSERT_TRUE(moduleBase != nullptr);
-    ASSERT_TRUE(moduleBase->mesosVersion != NULL) << "Module " << moduleName << " is missing field 'mesosVersion'";
-    ASSERT_TRUE(moduleBase->moduleApiVersion != NULL)
-        << "Module " << moduleName <<" is missing field 'moduleApiVersion'";
-    ASSERT_TRUE(moduleBase->authorName != NULL) << "Module " << moduleName << " is missing field 'authorName'";
-    ASSERT_TRUE(moduleBase->authorEmail != NULL) << "Module " << moduleName << " is missing field 'authoEmail'";
-    ASSERT_TRUE(moduleBase->description != NULL) << "Module " << moduleName << " is missing field 'description'";
-    ASSERT_TRUE(moduleBase->kind != NULL) << "Module " << moduleName << " is missing field 'kind'";
-
-    // Verify module API version.
-    EXPECT_TRUE(stringify(moduleBase->moduleApiVersion) == MESOS_MODULE_API_VERSION)
-        << "Module API version mismatch. Mesos has: " MESOS_MODULE_API_VERSION ", "
-        << "library requires: " + stringify(moduleBase->moduleApiVersion);
-
-    EXPECT_EQ("ResourceEstimator", stringify(moduleBase->kind));
-
-    Try<Version> mesosVersion = Version::parse(MESOS_VERSION);
-
-    Try<Version> moduleMesosVersion = Version::parse(moduleBase->mesosVersion);
-    EXPECT_FALSE(moduleMesosVersion.isError()) << moduleMesosVersion.error();
-
-    EXPECT_EQ(moduleMesosVersion.get(), mesosVersion.get())
-        << "Module is not compiled for the current Mesos version.";
-
-    EXPECT_TRUE(moduleBase->compatible()) << "Module " << moduleName << "has determined to be incompatible";
-}
-
-Try<ModuleBase*> ThresholdResourceEstimatorTest::loadModule() {
-    if (this->moduleBase == nullptr) {
-        auto const path = "./" + os::libraries::expandName(libraryName);
-        Try<Nothing> result = this->dynamicLibrary->open(path);
-        if (!result.isSome()) {
-            return Error("Error opening library of module: '" + moduleName + "': " + result.error());
-        }
-
-        Try<void*> symbol = this->dynamicLibrary->loadSymbol(moduleName);
-        if (symbol.isError()) {
-            return Error("Error loading module '" + moduleName + "': " + symbol.error());
-        }
-
-        this->moduleBase = reinterpret_cast<ModuleBase*>(symbol.get());
-    }
-    return moduleBase;
-}
-
-ResourceEstimator* ThresholdResourceEstimatorTest::createEstimator(mesos::Parameters const & params) {
-    auto load_result = this->loadModule();
-    ModuleBase* moduleBase = load_result.get();
-    auto estimatorModule = reinterpret_cast<Module<ResourceEstimator>*>(moduleBase);
-    return estimatorModule->create(params);
-}
-
 ResourceUsage noUsage() {
     return ResourceUsage{};
 }
@@ -151,7 +168,7 @@ TEST_F(ThresholdResourceEstimatorTest, test_load_library) {
     auto load_result = loadModule();
     ASSERT_FALSE(load_result.isError()) << load_result.error();
     ModuleBase* moduleBase = load_result.get();
-    verifyModule(moduleName, moduleBase);
+    verifyModule(estimatorModuleName, moduleBase);
     Owned<ResourceEstimator> estimator{createEstimator(make_parameters("", None(), None(), None(), None()))};
     ASSERT_NE(nullptr, estimator.get());
 }
@@ -176,7 +193,7 @@ TEST_F(ThresholdResourceEstimatorTest, test_below_thresholds) {
     EXPECT_EQ(512, available_resources.revocable().mem().get().megabytes());
 }
 
-TEST_F(ThresholdResourceEstimatorTest, test_above_load_threshold_1min) {
+TEST_F(ThresholdResourceEstimatorTest, test_above_thresholds) {
     Owned<ResourceEstimator> estimator{createEstimator(make_parameters(
         "cpus(*):2;mem(*):512", "0.0", "0.0", "0.0", "0"  // absurdly low load limit that will always be hit
     ))};
