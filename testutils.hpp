2ed69cc4a172842ca64005fbfef2bbb5a6caebcb
Stephan Erb <stephan.erb@blue-yonder.com>
2016-09-06 11:12:55 +0200

    Move fake/mock classes to separate file and test suite

---
+++ testutils.hpp
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <stout/os.hpp>
+#include "os.hpp"
+
+#include <mesos/resources.hpp>
+#include <process/process.hpp>
+
+using process::Future;
+
+using mesos::Resources;
+using mesos::ResourceUsage;
+
+using com::blue_yonder::os::MemInfo;
+
+namespace {
+
+
+class ResourceUsageFake {
+public:
+    ResourceUsageFake()
+        : value{std::make_shared<ResourceUsage>()}
+    {};
+
+    void set(
+        std::string const & revocable_allocated,
+        std::string const & non_revocable_allocated
+    ) {
+        value->Clear();
+        auto * revocable_executor = value->add_executors();
+        auto revocable_resources = Resources::parse(revocable_allocated);
+        for(auto const & parsed_resource: revocable_resources.get()) {
+            auto * mutable_resource = revocable_executor->add_allocated();
+            mutable_resource->CopyFrom(parsed_resource);
+            mutable_resource->mutable_revocable();  // mark as revocable
+        }
+
+        auto * non_revocable_executor = value->add_executors();
+        auto non_revocable_resources = Resources::parse(non_revocable_allocated);
+        for(auto const & parsed_resource: non_revocable_resources.get()) {
+            auto * mutable_resource = non_revocable_executor->add_allocated();
+            mutable_resource->CopyFrom(parsed_resource);
+            mutable_resource->clear_revocable();  // mark as non-revocable
+        }
+    }
+    Future<ResourceUsage> operator()() const {
+        return *value;
+    }
+private:
+    std::shared_ptr<ResourceUsage> value;
+};
+
+class LoadFake {
+public:
+    LoadFake() : value{std::make_shared<Try<os::Load>>(os::Load{0, 0, 0})} {};
+
+    Try<os::Load> operator()() const {
+        return *value;
+    }
+
+    void set(float one, float five, float fifteen) {
+        *value = os::Load{one, five, fifteen};
+    }
+
+    void set_error() {
+        *value = Error("Injected by Test");
+    }
+
+private:
+    std::shared_ptr<Try<os::Load>> value;
+};
+
+class MemInfoFake {
+public:
+    MemInfoFake() : value{std::make_shared<Try<MemInfo>>(MemInfo{0, 0, 0})} {};
+
+    Try<MemInfo> operator()() const {
+        return *value;
+    }
+
+    void set(std::string const & total, std::string const & free, std::string const & cached) {
+        *value = MemInfo{
+            Bytes::parse(total).get(),
+            Bytes::parse(free).get(),
+            Bytes::parse(cached).get()
+        };
+    }
+
+    void set_error() {
+        *value = Error("Injected by Test");
+    }
+
+private:
+    std::shared_ptr<Try<MemInfo>> value;
+};
+
+
+
+}
--- unit_test.cpp
+++ unit_test.cpp
@@ -1,7 +1,7 @@
 #include "threshold_resource_estimator.hpp"
 #include "threshold_qos_controller.hpp"
 
-
+#include "testutils.hpp"
 #include <stout/os.hpp>
 
 #include <gtest/gtest.h>
@@ -20,170 +20,15 @@ using com::blue_yonder::os::MemInfo;
 
 namespace {
 
-class ResourceUsageMock {
-public:
-    ResourceUsageMock()
-        : value{std::make_shared<ResourceUsage>()}
-    {};
-
-    void set(
-        std::string const & revocable_allocated,
-        std::string const & non_revocable_allocated
-    ) {
-        value->Clear();
-        auto * revocable_executor = value->add_executors();
-        auto revocable_resources = Resources::parse(revocable_allocated);
-        for(auto const & parsed_resource: revocable_resources.get()) {
-            auto * mutable_resource = revocable_executor->add_allocated();
-            mutable_resource->CopyFrom(parsed_resource);
-            mutable_resource->mutable_revocable();  // mark as revocable
-        }
-
-        auto * non_revocable_executor = value->add_executors();
-        auto non_revocable_resources = Resources::parse(non_revocable_allocated);
-        for(auto const & parsed_resource: non_revocable_resources.get()) {
-            auto * mutable_resource = non_revocable_executor->add_allocated();
-            mutable_resource->CopyFrom(parsed_resource);
-            mutable_resource->clear_revocable();  // mark as non-revocable
-        }
-    }
-    Future<ResourceUsage> operator()() const {
-        return *value;
-    }
-private:
-    std::shared_ptr<ResourceUsage> value;
-};
-
-class LoadMock {
-public:
-    LoadMock() : value{std::make_shared<Try<os::Load>>(os::Load{0, 0, 0})} {};
-
-    Try<os::Load> operator()() const {
-        return *value;
-    }
-
-    void set(float one, float five, float fifteen) {
-        *value = os::Load{one, five, fifteen};
-    }
-
-    void set_error() {
-        *value = Error("Injected by Test");
-    }
-
-private:
-    std::shared_ptr<Try<os::Load>> value;
-};
-
-class MemInfoMock {
-public:
-    MemInfoMock() : value{std::make_shared<Try<MemInfo>>(MemInfo{0, 0, 0})} {};
-
-    Try<MemInfo> operator()() const {
-        return *value;
-    }
-
-    void set(std::string const & total, std::string const & free, std::string const & cached) {
-        *value = MemInfo{
-            Bytes::parse(total).get(),
-            Bytes::parse(free).get(),
-            Bytes::parse(cached).get()
-        };
-    }
-
-    void set_error() {
-        *value = Error("Injected by Test");
-    }
-
-private:
-    std::shared_ptr<Try<MemInfo>> value;
-};
 
 
-TEST(ResourceUsageMockTest, test_set) {
-    ResourceUsageMock mock;
-    mock.set("cpus:3;mem:62", "cpus:1;mem:48");
 
-    ResourceUsage usage = mock().get();
-    Resources allocatedRevocable;
-    Resources allocatedNonRevocable;
-    for(auto & executor: usage.executors()) {
-        allocatedRevocable += Resources(executor.allocated()).revocable();
-        allocatedNonRevocable += Resources(executor.allocated()).nonRevocable();
-    }
-
-    EXPECT_EQ(2, usage.executors_size());
-    EXPECT_EQ(3, allocatedRevocable.cpus().get());
-    EXPECT_EQ(62, allocatedRevocable.mem().get().megabytes());
-    EXPECT_EQ(1, allocatedNonRevocable.cpus().get());
-    EXPECT_EQ(48, allocatedNonRevocable.mem().get().megabytes());
-
-    // must also work for copy
-    ResourceUsageMock copy = mock;
-    mock.set("cpus:5;mem:127", "");
-    usage = copy().get();
-
-    Resources copiedRevocable;
-    for(auto & executor: usage.executors()) {
-        copiedRevocable += Resources(executor.allocated()).revocable();
-    }
-    EXPECT_EQ(5, copiedRevocable.cpus().get());
-    EXPECT_EQ(127, copiedRevocable.mem().get().megabytes());
-}
-
-#define EXPECT_LOAD(expect_one, expect_five, expect_fifteen, load) \
-    do { \
-        EXPECT_EQ(expect_one, load.one); \
-        EXPECT_EQ(expect_five, load.five); \
-        EXPECT_EQ(expect_fifteen, load.fifteen); \
-    } while(false); \
-
-TEST(LoadMockTest, test_set) {
-    LoadMock mock;
-    EXPECT_LOAD(0, 0, 0, mock().get());
-
-    auto copy = mock;
-    mock.set(1.5, 2.5, 3.5);
-    EXPECT_LOAD(1.5, 2.5, 3.5, mock().get());
-    EXPECT_LOAD(1.5, 2.5, 3.5, copy().get());
-}
-
-TEST(LoadMockTest, test_set_error) {
-    LoadMock mock;
-
-    auto copy = mock;
-    mock.set_error();
-    EXPECT_TRUE(copy().isError());
-}
-
-TEST(MemInfoMockTest, test_default) {
-    MemInfoMock mock;
-    auto const mem = mock().get();
-    EXPECT_EQ(0, mem.total.bytes());
-    EXPECT_EQ(0, mem.free.bytes());
-}
-
-TEST(MemInfoMockTest, test_set) {
-    MemInfoMock mock;
-    auto copy = mock;
-    mock.set("1GB", "128MB", "256MB");
-    auto const mem = copy().get();
-    EXPECT_EQ(1, mem.total.gigabytes());
-    EXPECT_EQ(128, mem.free.megabytes());
-    EXPECT_EQ(256, mem.cached.megabytes());
-}
-
-TEST(MemInfoMockTest, test_set_error) {
-    MemInfoMock mock;
-    auto copy = mock;
-    mock.set_error();
-    EXPECT_TRUE(copy().isError());
-}
 
 
 struct ThresholdResourceEstimatorTests : public ::testing::Test {
-    ResourceUsageMock usage;
-    LoadMock load;
-    MemInfoMock memory;
+    ResourceUsageFake usage;
+    LoadFake load;
+    MemInfoFake memory;
     ThresholdResourceEstimator estimator;
 
     ThresholdResourceEstimatorTests(
@@ -207,9 +52,9 @@ struct ThresholdResourceEstimatorTests : public ::testing::Test {
 
 
 struct ThresholdQoSControllerTests : public ::testing::Test {
-    ResourceUsageMock usage;
-    LoadMock load;
-    MemInfoMock memory;
+    ResourceUsageFake usage;
+    LoadFake load;
+    MemInfoFake memory;
     ThresholdQoSController controller;
 
     ThresholdQoSControllerTests(
